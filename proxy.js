db723d58496cdcb07cb2826c7cedd0922a31ad68
Michael Durfey <mdurfey@me.com>
2018-03-05 17:15:35 -0800

    basic proxy complete

--- proxy.js
+++ proxy.js
@@ -2,7 +2,6 @@ const express = require('express');
 const morgan = require('morgan');
 const request = require('request');
 const path = require('path');
-const router = require('./router');
 const url = require('url');
 
 const app = express();
@@ -14,9 +13,6 @@ app.set('port', process.env.PORT || 3004);
 app.use(express.static(path.join(__dirname)));
 app.use(express.static(path.join(__dirname, '../title-map/client/dist')));
 
-// app.use('/', router);
-
-
 app.get('/title/:id', (req, res) => {
     request({
       url: `http://localhost:3001${req.path}`,
@@ -24,15 +20,11 @@ app.get('/title/:id', (req, res) => {
     }).pipe(res)
 });
 app.get('/map/:id', (req, res) => {
-  console.log(req.params.id)
-  request({
-  url: `http://localhost:3001${req.path}`,
-  method: req.method,
-}).pipe(res)
+    request({
+    url: `http://localhost:3001${req.path}`,
+    method: req.method,
+  }).pipe(res)
 });
 
-app.put('/:id', (req, res) => {
-  console.log(req.body)
-})
 
 app.listen(app.get('port'), () => console.log(`sever listening on ${app.get('port')}`));
