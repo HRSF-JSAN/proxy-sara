20a44d2a840056e655f78ed8789b46ca9d6a491f
Michael Durfey <mdurfey@me.com>
2018-03-09 20:00:39 -0800

    set view folder

--- proxy.js
+++ proxy.js
@@ -6,6 +6,8 @@ const app = express();
 
 app.use(parse.urlencoded({ extended: false }));
 app.use(parse.json());
+app.use(express.static('views'));
+app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.use('/', router);
